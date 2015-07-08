17462883cbb48e7c30339377a3eda189d023bfef
Ben Foster <ben@planetcloud.co.uk>
2015-07-08 09:54:38 +0100

    Added Mvc sample and extensions for HttpRequestBase.

---
+++ samples/SaasKit.Multitenancy.Samples.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SaasKit.Multitenancy.Samples.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var tenant = Request.GetTenant<AppTenant>();
+            return View(tenant);
+        }
+    }
+}
---
+++ samples/SaasKit.Multitenancy.Samples.Mvc/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Owin;
+using System;
+using System.Threading.Tasks;
+
+namespace SaasKit.Multitenancy.Samples.Mvc
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseMultitenancy(new AppTenantResolver());
+        }
+    }
+
+    public class AppTenant
+    {
+        public string Name { get; set; }
+    }
+
+    public class AppTenantResolver : UriTenantResolver<AppTenant>
+    {
+        public override Task<TenantContext<AppTenant>> ResolveAsync(Uri uri)
+        {
+            AppTenant tenant = null;
+
+            if (uri.Host.Equals("dev.local", StringComparison.OrdinalIgnoreCase))
+            {
+                tenant = new AppTenant { Name = "Tenant 2" };
+            }
+            else
+            {
+                tenant = new AppTenant { Name = "Tenant 1" };
+            }
+
+            return Task.FromResult(new TenantContext<AppTenant>(tenant));
+        }
+    }
+}
