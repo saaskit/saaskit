f3336d17c7896d12181a81e39d48bc0f5f2e7747
Ben Foster <ben@planetcloud.co.uk>
2015-07-22 10:57:07 +0100

    Bumped version number.

--- src/SaasKit.Multitenancy.Mvc/Properties/AssemblyInfo.cs
+++ src/SaasKit.Multitenancy.Mvc/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
-[assembly: AssemblyInformationalVersion("0.0.1-pre")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: AssemblyInformationalVersion("0.0.2-pre")]
