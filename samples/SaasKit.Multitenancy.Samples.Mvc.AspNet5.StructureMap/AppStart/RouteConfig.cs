3ec13d91c6e3299046617f8dbdb656e26e95d7c2
saan800 <sandragreenhalgh@gmail.com>
2015-11-12 01:28:40 +0000

    add vnext + SaasKit + StructureMap helper project and sample app

---
+++ samples/SaasKit.Multitenancy.Samples.Mvc.AspNet5.StructureMap/AppStart/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Routing;
+
+
+namespace SaasKit.Multitenancy.Samples.Mvc.AspNet5.StructureMap.AppStart
+{
+	public static class RouteConfig
+	{
+		public static IRouteBuilder Setup(IRouteBuilder routes)
+		{
+			routes.MapRoute(
+				name: "default",
+				template: "{controller=Home}/{action=Index}/{id?}"
+			);
+
+			return routes;
+		}
+	}
+}
