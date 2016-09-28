279ee213ee9f52cb6ca3efe5934e72b71d7e40c6
Steven Knox <steven@wholeschool.ie>
2016-09-28 12:37:00 +0100

    Add ITenant extension to enable local creation of object

---
+++ tests/SaasKit.Multitenancy.Tests/ITenantExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace SaasKit.Multitenancy.Tests
+{
+    public class ITenantExtensionsTests
+    {
+        [Fact]
+        public void Can_Create_Local_Tenant()
+        {
+            var tenantName = "Tenant 1";
+
+            ITenant<AppTenant> tenant = ITenant.Create(new AppTenant { Name = tenantName });
+
+            Assert.Equal(tenantName, tenant.Value.Name);
+        }
+    }
+
+    public class AppTenant
+    {
+        public string Name { get; set; }
+    }
+}
